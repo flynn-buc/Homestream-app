c12ca19a5fc76efd735e4604196133573750767a
Jonathan Hirsch <jonat1996@gmail.com>
2020-07-16 16:11:59 -0700

    Initial Commit

---
+++ HomeStreaming-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeStreaming-app
+//
+//  Created by Jonathan Hirsch on 7/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

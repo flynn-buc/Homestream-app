42acb95eaa044c1f56485d17cc04d9bf664e0a2f
Jonathan Hirsch <jonat1996@gmail.com>
2020-07-17 15:02:00 -0700

    Working on UI display of folders

---
+++ HomeStreaming-app/Model/APIError.swift
@@ -0,0 +1,8 @@
+//
+//  APIError.swift
+//  HomeStreaming-app
+//
+//  Created by Jonathan Hirsch on 7/17/20.
+//
+
+import Foundation
---
+++ HomeStreaming-app/Model/Subfolders.swift
@@ -0,0 +1,8 @@
+//
+//  Subfolders.swift
+//  HomeStreaming-app
+//
+//  Created by Jonathan Hirsch on 7/17/20.
+//
+
+import Foundation
